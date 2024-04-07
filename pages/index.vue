6b9b41d5e8b6db55fe3f1ed9a1c2c48a4ce61c74
DrxcoDev <drxcovalorant@gmail.com>
2024-04-07 18:11:23 +0200

    💥New content

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,84 @@
 
 <template>
 
-    <div style="padding: 60px;width: 1000px;height: 600px;background: black;" class="position-absolute top-50 start-50 translate-middle rounded shadow-lg text-light">
+    <div id="div1"style="padding: 60px;width: 1000px;height: 600px;background: black;margin-bottom: 40px;" class="div visible position-absolute top-50 start-50 translate-middle rounded shadow-lg text-light">
         <div class="d-flex position-absolute top-50 start-50 translate-middle">
             <h1 class="">Welcome to</h1><h1 class="text-warning ml-2" style="margin-left: 20px;">Zest</h1>
         </div>
         <div class="position-absolute top-50 start-50 translate-middle" style="margin-top: 200px;">
+            <button onclick="cambiarDiv()" class="btn">
+                <a href="#div2">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-bar-down" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M1 3.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5M8 6a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 0 1 .708-.708L7.5 12.293V6.5A.5.5 0 0 1 8 6"/>
+                    </svg>
+                </a>
+                
+            </button>
+            
+        </div>
+        
+    </div>
+
+    <div id="div2"style="padding: 60px;width: 1000px;height: 600px;background: black;margin-top: 700px;margin-bottom: 200px;" class="div position-absolute top-50 start-50 translate-middle rounded shadow-lg text-light">
+        <h1 class="">Fast & easy</h1>
+        <div class="d-flex">
+            <p style="margin-top: 100px;width: 400px;">Zest is a web application that allows you to recreate statistics quickly and with real-time data, it can be edited and configured at the moment.</p>
+            <div class="image" style="align-items: center;width: 50px;height: 50px;margin-left: 100px;">
+            
+                <svg xmlns="http://www.w3.org/2000/svg" width="300px" height="300px" viewBox="0 0 146 146"><title>001-business</title><rect x="0.5" y="0.5" width="145" height="145" fill="none"/><circle cx="23.35" cy="69.53" r="4.8" fill="#a8cbeb"/><circle cx="109.75" cy="34.33" r="4.8" fill="#a8cbeb"/><circle cx="80.95" cy="58.33" r="4.8" fill="#a8cbeb"/><path d="M48,49.61a4.67,4.67,0,0,1-.7-2.48A4.81,4.81,0,1,1,48,49.61Z" fill="#a8cbeb"/><path d="M118.55,53.53v52.8h-17.6V53.53a3.2,3.2,0,0,1,3.2-3.2h11.2A3.2,3.2,0,0,1,118.55,53.53Z" fill="#ea9080"/><path d="M60.95,66.33v40H43.35v-40a3.2,3.2,0,0,1,3.2-3.2h11.2A3.2,3.2,0,0,1,60.95,66.33Z" fill="#7bd489"/><path d="M14.55,106.33V88.73a3.2,3.2,0,0,1,3.2-3.2h11.2a3.2,3.2,0,0,1,3.2,3.2v17.6" fill="none" stroke="#515f76" stroke-miterlimit="10" stroke-width="2.4"/><path d="M72.15,106.33V77.53a3.2,3.2,0,0,1,3.2-3.2h11.2a3.2,3.2,0,0,1,3.2,3.2v28.8" fill="#ffffff" stroke="#515f76" stroke-miterlimit="10" stroke-width="2.4"/><path d="M56.95,47.13A4.8,4.8,0,0,1,48,49.61a4.67,4.67,0,0,1-.7-2.48A4.8,4.8,0,1,1,56.95,47.13Z" fill="none" stroke="#515f76" stroke-linecap="round" stroke-miterlimit="10" stroke-width="2.4"/><circle cx="109.75" cy="34.33" r="4.8" fill="none" stroke="#515f76" stroke-linecap="round" stroke-miterlimit="10" stroke-width="2.4"/><circle cx="80.95" cy="58.33" r="4.8" fill="none" stroke="#515f76" stroke-linecap="round" stroke-miterlimit="10" stroke-width="2.4"/><circle cx="23.35" cy="69.53" r="4.8" fill="none" stroke="#515f76" stroke-linecap="round" stroke-miterlimit="10" stroke-width="2.4"/><polyline points="27.5 66.33 48.04 49.6 48.15 49.52" fill="none" stroke="#515f76" stroke-miterlimit="10" stroke-width="2.4"/><line x1="56.95" y1="48.73" x2="76.15" y2="56.59" fill="none" stroke="#515f76" stroke-miterlimit="10" stroke-width="2.4"/><line x1="85.19" y1="55.79" x2="105.68" y2="37.84" fill="none" stroke="#515f76" stroke-miterlimit="10" stroke-width="2.4"/><path d="M115.35,50.4h-11.2a3.54,3.54,0,0,0-3.53,3.53v52.39l2.4,0.07L103.38,57a2.4,2.4,0,0,1,2-2.36l13-1.12h0A3,3,0,0,0,115.35,50.4Z" fill="#eeb8a9" opacity="0.8"/><path d="M100.95,106.33V53.53a3.2,3.2,0,0,1,3.2-3.2h11.2a3.2,3.2,0,0,1,3.2,3.2v52.8" fill="none" stroke="#515f76" stroke-miterlimit="10" stroke-width="2.4"/><path d="M57.75,63.2H46.55A3.41,3.41,0,0,0,43,66.29l0,40.11h2.43V70.46a2.4,2.4,0,0,1,2.39-2.4l13-1.7A3.19,3.19,0,0,0,57.75,63.2Z" fill="#a4e0b3" opacity="0.8"/><path d="M43.35,106.33v-40a3.2,3.2,0,0,1,3.2-3.2h11.2a3.2,3.2,0,0,1,3.2,3.2v40" fill="none" stroke="#515f76" stroke-miterlimit="10" stroke-width="2.4"/><line x1="2.75" y1="113.67" x2="5.95" y2="116.87" fill="none" stroke="#b8b9ba" stroke-linecap="round" stroke-miterlimit="10" stroke-width="1.6"/><line x1="5.95" y1="113.67" x2="2.75" y2="116.87" fill="none" stroke="#b8b9ba" stroke-linecap="round" stroke-miterlimit="10" stroke-width="1.6"/><circle cx="121.15" cy="115.27" r="1.6" fill="#b8b9ba"/><circle cx="8.75" cy="69.27" r="2" fill="none" stroke="#b8b9ba" stroke-linecap="round" stroke-miterlimit="10" stroke-width="1.6"/><circle cx="13.95" cy="60.07" r="1.6" fill="#b8b9ba"/><path d="M140.05,99.36l2.33-1.22a0.08,0.08,0,0,1,.12.08l-0.44,2.59,2,1.95-2.76.4-1.24,2.5-1.24-2.5-2.6-.38a0.08,0.08,0,0,1,0-.14l1.88-1.84-0.47-2.75Z" fill="#a8cbeb"/><circle cx="134.75" cy="89.67" r="1.6" fill="#b8b9ba"/><circle cx="133.55" cy="114.87" r="2" fill="none" stroke="#b8b9ba" stroke-linecap="round" stroke-miterlimit="10" stroke-width="1.6"/><line x1="8.15" y1="106.33" x2="126.55" y2="106.33" fill="none" stroke="#515f76" stroke-linecap="round" stroke-miterlimit="10" stroke-width="2.4"/></svg>
+            
+            </div>
+        </div>
+        
+        <div class="position-absolute top-50 start-50 translate-middle" style="margin-top: 200px;">
             <button class="btn">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-bar-down" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M1 3.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5M8 6a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 0 1 .708-.708L7.5 12.293V6.5A.5.5 0 0 1 8 6"/>
-                </svg>
+                <a href="">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-bar-down" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M1 3.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5M8 6a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 0 1 .708-.708L7.5 12.293V6.5A.5.5 0 0 1 8 6"/>
+                    </svg>
+                </a>
+                
             </button>
             
         </div>
         
     </div>
 </template>
-  
+
+<style>
+
+.visible {
+    opacity: 1;
+}
+
+.div {
+    transition: opacity 0.5s ease;
+}
+
+.invisible {
+    opacity: 0;
+}
+
+</style>
+
+<script>
+
+function cambiarDiv() {
+    var div1 = document.getElementById("div1");
+    var div2 = document.getElementById("div2");
+
+    if (div1.classList.contains("visible")) {
+        div1.classList.remove("visible");
+        div1.classList.add("invisible");
+        div2.classList.remove("invisible");
+        div2.classList.add("visible");
+    } else {
+        div2.classList.remove("visible");
+        div2.classList.add("invisible");
+        div1.classList.remove("invisible");
+        div1.classList.add("visible");
+    }
+}
+
+
+</script>
