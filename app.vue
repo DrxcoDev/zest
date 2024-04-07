a0d1c42dd891daaacde265e3c7e4fa43cd7c87cc
DrxcoDev <drxcovalorant@gmail.com>
2024-04-07 13:05:02 +0200

    A commit

--- app.vue
+++ app.vue
@@ -1,5 +1,21 @@
+
+<script>
+import index from './pages/index.vue'
+
+export default {
+    components: {
+        index
+    }
+}
+
+
+</script>
+
+
+
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <body class="bg-dark">
+    <index />
+
+  </body>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+
+<template>
+
+    <div style="padding: 60px;width: 1000px;height: 600px;background: black;" class="position-absolute top-50 start-50 translate-middle rounded shadow-lg text-light">
+        <div class="d-flex position-absolute top-50 start-50 translate-middle">
+            <h1 class="">Welcome to</h1><h1 class="text-warning ml-2" style="margin-left: 20px;">Zest</h1>
+        </div>
+        <div class="position-absolute top-50 start-50 translate-middle" style="margin-top: 200px;">
+            <button class="btn">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-bar-down" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M1 3.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5M8 6a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 0 1 .708-.708L7.5 12.293V6.5A.5.5 0 0 1 8 6"/>
+                </svg>
+            </button>
+            
+        </div>
+        
+    </div>
+</template>
+  
